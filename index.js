75aaa7ee7b496dacf5cc761cb47a84b12ee8ec21
Utkarsh Ghanwat <utkarshghanwat45@gmail.com>
2025-08-29 19:17:14 +0530

    Your commit message

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { console } = require("inspector");
 const app = express();
 const port = 8080;
 const path = require("path");
-
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -31,27 +31,44 @@ const posts = [
     },
 ]; 
 
-app.get("/posts",(req, res) =>{
+app.get("/posts",(req, res) =>{            ///INDEX
     res.render("index.ejs",{ posts });
 });
 
-app.get("/posts/new",(req, res) => {
+app.get("/posts/new",(req, res) => {     ///ADD NEW POST
     res.render("new.ejs");
 })
 
-app.post("/posts", (req, res)=>{
+app.post("/posts", (req, res)=>{          ///CREATE ID 
     let { username , content }=req.body;
     let id = uuidv4();
     posts.push({id, username, content});
-    res.redirect("/posts");               // REDIRECT
+    res.redirect("/posts");               /// REDIRECT
 });
 
-app.get("/posts/:id", (req, res)=>{
+app.get("/posts/:id", (req, res)=>{       ///  DETAIL OF 1 POST
     let { id }=req.params;
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs" , {post});
 });
-app.listen (port, () => {
+
+app.patch("/posts/:id", (req, res)=>{            /// UPDATE
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id ===p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+}) 
+
+app.get("/posts/:id/edit", (req, res)=>{            /// EDIT
+    let {id} = req.params;
+    let post = posts.find((p) => id ===p.id);
+    res.render("edit.ejs", {post});
+}) 
+
+
+app.listen (port, () => {                             /// GEN
     console.log("listening to port : 8080");
 }); 
 
